f55e302f04ef026b6a061b3e461db356ad1d482b
Yarkov <yurayarkov@gmail.com>
2018-12-05 00:34:43 +0300

    EDIT

--- views.py
+++ views.py
@@ -1,7 +1,20 @@
 from app import app
 
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
 
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 tasks = [
     {
@@ -18,13 +31,24 @@ tasks = [
     }
 ]
 
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task[field] = task[field]
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': list(map(make_public_task, tasks))})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
@@ -33,7 +57,7 @@ def get_task(task_id):
 
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
@@ -75,6 +99,7 @@ def delete_task(task_id):
     tasks.remove(task[0])
     return jsonify({'result': True})
 
-#@app.route('/')
-#def hello_world():
-#    return 'Hello World!'
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
