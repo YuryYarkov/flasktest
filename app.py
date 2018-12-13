febd6e06e4427d4391b028e3260bbd5b6cce2a84
Yarkov <yurayarkov@gmail.com>
2018-12-14 00:17:17 +0300

    правка предупреждений

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify, abort, make_response, request
 from flask_restful import Api, Resource
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__, static_url_path = "")
 
 import views
 
 
-
+app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
